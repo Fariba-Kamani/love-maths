a5a5964b588133f0fb216a06dbe34bc8ade54465
Seyedehfariba Kamani <fariba.kamani87@gmail.com>
2024-09-16 13:31:18 +0000

    Set up the structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAddistionQuestion() {
+
+}
+
+function displaySubtractionQuestion() {
+
+}
+
+function displayMultipyQuestion() {
+
+}
+
+function displayDivisionQuestion() {
+
+}
